8f42855a82139d80494984d9c8db1419b2d1813a
Rizwan Ashraf <riz1ash786@gmail.com>
2022-05-31 19:36:39 +0100

    initial file structure and app setup

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+/*
+Challenge: Build the Navbar component.
+Check the Figma file for the design specifics.
+*/
+
+export default function App() {
+  return <h1>App component</h1>;
+}
